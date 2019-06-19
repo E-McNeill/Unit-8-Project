bdc22a81f37d08d2fd14a093f5e73b208bc41a1c
E-McNeill <ericaksaunders@gmail.com>
2019-06-19 11:06:36 -0700

    main rendered

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
+var bookroutes = require('./books');
+router.use('/', bookroutes);
+
 // GET Home (redirect to books) also found on index
 router.get('/', (req, res) => {
     res.redirect('/books');
