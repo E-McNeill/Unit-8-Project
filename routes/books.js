6b8922f94f3058841d08ea1e6bdb6a5703454f60
E-McNeill <ericaksaunders@gmail.com>
2019-06-19 14:05:50 -0700

    update-book working

--- routes/books.js
+++ routes/books.js
@@ -4,11 +4,6 @@ const Book = require("../models").Book;
 const Sequelize = require("sequelize");
 
 // //GET books (shows full list of books)
-// router.get('/books', (req, res) => {
-//     res.render('index');
-// });
-
-/* GET articles listing. */
 router.get('/books', function(req, res, next) {
     Book.findAll({order: [["Year", "DESC"]]}).then(function(books){
       res.render('index', {books: books, title: 'All Books' });
@@ -20,13 +15,26 @@ router.get('/books/new', (req, res) => {
     res.render('new-book');
 });
 
+// // GET books ID (shows book details on click)
+// router.get('/books/update', (req, res) => {
+//     res.render('update-book');
+// });
+
+router.get("/books/:id", function(req, res, next){
+    Book.findByPk(req.params.id).then(function(books){
+      res.render("update-book", {books: books, title: books.title});
+    });
+  });
+
+  
+
 //Error
 router.get('/error', (req, res) => {
     res.render('error');
 });
 
 // POST books new (posts new book to the database)
-// GET books ID (shows book details on click)
+
 // POST books ID (Updates book info in database)
 // POSt books ID DELETe (remove book)
 
